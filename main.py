d3f9698f3e6159cb90c36f9c616df4fdf6826ee0
Ndolo Mercy <mercymwendend444@gmail.com>
2025-10-19 23:01:44 +0300

    Add initial model files and update main.py to streamline execution

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # main.py
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# import sys
+# import os
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from lib.database import create_tables
-from lib.cli import main_menu
+# from lib.database import create_tables
+# from lib.cli import main_menu
+
+# if __name__ == "__main__":
+#     create_tables()     
+#     main_menu()         
+from lib.cli import main
 
 if __name__ == "__main__":
-    create_tables()     
-    main_menu()         
+    main()
